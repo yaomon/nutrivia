9b77d081a46ef2de53aeb2fd98e49d68a7e85675
yaomon <yaomon18@yahoo.com>
2024-07-17 22:33:08 -0700

    [NT}Add stat tracking

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,36 @@
 (function ($) {
     "use strict";
     let curr_ques;
+    let missed_ques = [];
+    let missed_ques_counter = 0;
+    let answered_ques = new Set([]);
+    let total_ques = domain1_answers.length;
+    let num_corr = 0;
+    let num_incorr = 0;
     // Function to get a random integer within a range
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     function updateQuestion() {
+        missed_ques_counter++;
+        // Every 5 questions, try to re-ask a missed question
         // Get a random index within the array length
-        const randomIndex = getRandomInt(0, domain1_answers.length - 1);
+        let randomIndex = getRandomInt(0, domain1_answers.length - 1);
+        if (missed_ques_counter > 4) {
+            missed_ques_counter = 0;
+            if (missed_ques.length > 0) {
+                // Override the index with one of the missed questions
+                const randomMissedQuesIndex = getRandomInt(
+                    0,
+                    missed_ques.length - 1
+                );
+                // Remove the missed question from the array
+                randomIndex = parseInt(missed_ques[randomMissedQuesIndex]);
+                missed_ques.splice(randomMissedQuesIndex, 1);
+            }
+        }
+
         // Retrieve the random object
         const randomQuestion = domain1_answers[randomIndex];
         curr_ques = randomQuestion;
@@ -28,14 +50,25 @@
     function guessAnswer() {
         $(".choice").addClass("disabled");
         let guess = $(this).find(".choice-let").text();
+        let ques_number = curr_ques.number;
+        answered_ques.add(ques_number);
+        $(".question-frac").text(answered_ques.size + "/" + total_ques);
         if (guess !== curr_ques.correct_answer) {
             // Incorrect
             $(this).addClass("incorrect");
-            $(".incorrect-num").text(parseInt($(".incorrect-num").text()) + 1);
+            num_incorr++;
+            $(".incorrect-num").text(num_incorr);
+            // Add the missed question to the missed questions
+            // Subtract 1 to convert to 0 based index
+            missed_ques.push(ques_number - 1);
+            missed_ques.push(ques_number - 1);
         } else {
             // Correct
-            $(".correct-num").text(parseInt($(".correct-num").text()) + 1);
+            num_corr++;
+            $(".correct-num").text(num_corr);
         }
+        let perc = num_corr / (num_corr + num_incorr);
+        $(".stat-perc").text(Math.round(perc * 100) + "%");
         // Add correct answer
         switch (curr_ques.correct_answer) {
             case "a":
@@ -55,6 +88,7 @@
 
     $(document).ready(function () {
         // Code to run when the document is ready
+        $(".question-frac").text(answered_ques.size + "/" + total_ques);
         updateQuestion();
         $(".skip").click(updateQuestion);
         $(".choice").click(guessAnswer);
